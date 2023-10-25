3f7aa7eed90714e9b1994f75e79c7a74518b1021
chenyukb <92746141+chenyukb@users.noreply.github.com>
2023-10-25 09:55:13 +0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("这是main分支。");
+    System.out.println("main分支前进了一个commit。");
   }
 
 }

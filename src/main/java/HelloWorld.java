840c17bd1a031336dee5a1496329837477f92056
chenyukb <92746141+chenyukb@users.noreply.github.com>
2023-10-25 09:58:29 +0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,7 @@
 public class HelloWorld {
 
   public static void main(String[] args) {
+    System.out.println("dev分支。 开发的需求新提交");
     System.out.println("Hello World!");
     System.out.println("这是main分支。");
   }
